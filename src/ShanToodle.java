0433c69c88271f4e458bd8f3b45703d651eb1256
Minghao Shan <minghao8shan@gmail.com>
2017-02-27 20:36:20 -0600

    second commit

---
+++ src/ShanToodle.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author minghao
+ *
+ */
+public class ShanToodle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
