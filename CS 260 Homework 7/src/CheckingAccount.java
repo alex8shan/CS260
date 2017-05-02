0e29df400ee38720b90c8fb65b1eec78ab430733
Minghao Shan <minghao8shan@gmail.com>
2017-05-02 05:56:31 -0500

    finished half of them

--- CS 260 Homework 7/src/CheckingAccount.java
+++ CS 260 Homework 7/src/CheckingAccount.java
@@ -1,12 +1,38 @@
 
 public class CheckingAccount extends BankAccount{
-	public CheckingAccount(){
-		
+	private double withdrawFee;
+	private int freeDepoCount;
+	private int freeDepositeCount;
+	private double depositeFee;
+	private double overdraftFee;
+	private double accountBalance;
+
+	public CheckingAccount(double withdrawFee, int freeDepositeCount, double depositeFee, double overdraftFee ){
+		this.withdrawFee = withdrawFee;
+		this.freeDepoCount = freeDepositeCount;
+		this.freeDepositeCount = freeDepoCount;
+		this.depositeFee = depositeFee;
+		this.overdraftFee = overdraftFee;
+		accountBalance = 0.00;
+	}
+	public CheckingAccount(double initBalance, double withdrawFee, int freeDepositeCount, double depositeFee, double overdraftFee ){
+		this.withdrawFee = withdrawFee;
+		this.freeDepoCount = freeDepositeCount;
+		this.freeDepositeCount = freeDepoCount;
+		this.depositeFee = depositeFee;
+		this.overdraftFee = overdraftFee;
+		this.accountBalance = initBalance;
 	}
 	public void deductDepositFees(){
-		
+		if(freeDepositeCount > 0){
+			accountBalance = accountBalance - depositeFee;
+		}
+		freeDepositeCount --;
 	}
 	public void deductWithdrawalFees(){
-		
+		accountBalance = accountBalance - withdrawFee;
+	}
+	public void ResetCounters(){
+		freeDepositeCount = freeDepoCount;
 	}
 }
---
+++ CS 260 Homework 7/src/VIPCheckingAccount.java
@@ -0,0 +1,10 @@
+
+public class VIPCheckingAccount extends CheckingAccount{
+	//account?
+	
+	public VIPCheckingAccount(double withDrawFee, int extraFreeDepositeCount,double depositeFee, double overDraftFee ){
+		//balance = super.getBalance();
+		//FreeDepositeCount = 30 + extraFreedDepositeCount;
+		
+	}
+}
