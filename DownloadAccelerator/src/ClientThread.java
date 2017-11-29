560be225a561780f7cefc57eafdb7b734a1a4906
Minghao Shan <minghao8shan@gmail.com>
2017-11-29 07:50:17 -0600

    Update comments

---
+++ DownloadAccelerator/src/ClientThread.java
@@ -0,0 +1,10 @@
+
+public class ClientThread {
+	//wait for file to download -> await termination
+	
+	
+	
+	//1000 bytes to 10000 bytes.
+	//byte buffer array of 1000 bytes
+
+}
--- DownloadAccelerator/src/ServerThread.java
+++ DownloadAccelerator/src/ServerThread.java
@@ -3,6 +3,8 @@ public class ServerThread implements Runnable{
 
 	@Override
 	public void run() {
+		//5 server sockets
+		//each sockets have 5 port numbers
 		
 		
 	}
